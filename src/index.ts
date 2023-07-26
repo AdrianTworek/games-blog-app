364648a5ea667a700c15b376328f9740135738de
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-07-26 23:18:02 +0200

    Install zod, zennv and setup typesafe env variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { createServer } from 'http';
 import { config } from 'dotenv';
 import { app } from './app';
+import { env } from './config/env';
 
 config();
 
-const PORT = Number(process.env.PORT ?? 5000);
-const HOST = '0.0.0.0';
+const PORT = env.PORT ?? 5000;
+const HOST = env.HOST;
 
 const server = createServer(app);
 
