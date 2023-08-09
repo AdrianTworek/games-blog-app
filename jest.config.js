a5aa3f04749b5b934f028e5150cc963ce23ab3d8
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-08-09 18:17:38 +0200

    Setup testing environment

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/*eslint no-undef: off */
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/db/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+};
