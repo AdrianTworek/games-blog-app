26dffe67444af333e5b1f698a9574577c4b1927a
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-08-12 18:19:10 +0200

    Apply security middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+import helmet from 'helmet';
 import { api } from '@src/routes';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 250,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
+app.use(cors());
+app.use(helmet());
 app.use('/api', api);
 
 export { app };
