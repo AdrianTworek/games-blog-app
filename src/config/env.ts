79fc185d09b996cf491273358feeb603fd91c379
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-08-12 19:36:45 +0200

    Update config with JWT secrets

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,5 +7,7 @@ export const env = zennv({
     PORT: z.number().default(5000),
     HOST: z.string().default('0.0.0.0'),
     DATABASE_URL: z.string(),
+    ACCESS_TOKEN_SECRET: z.string(),
+    REFRESH_TOKEN_SECRET: z.string(),
   }),
 });
