0a87dab31e8b54b512fcb821160c56cc463e6877
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-08-26 14:51:10 +0200

    Fix deserializeUser middleware

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -32,7 +32,7 @@ export const deserializeUser = (
       );
     }
 
-    req.user.email = decoded.email;
+    req.user = { email: decoded.email };
     next();
   } catch (error) {
     next(error);
